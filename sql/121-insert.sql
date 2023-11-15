0910825584eba805b59cfd599d2f8c030b65f61a
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-15 17:53:31 +0200

    sql/121: modify homework

--- sql/121-insert.sql
+++ sql/121-insert.sql
@@ -18,10 +18,10 @@
 */
 
 
-INSERT INTO person(id, last, first, phone, zip, city, address)
-VALUES (1, 'Doe', 'John', 123455, 11122, 'New York', '2th avenue'),
-       (2, 'Jordan', 'Mike', 221122, 01212, 'Washington', 'South Park 3'),
-       (3, 'Durak', 'Stephen', 550011, 55654, 'Miami', 'Sea Drive 112');
+INSERT INTO person(last, first, phone, zip, city, address)
+VALUES ('Doe', 'John', '123-456-7890', 11122, 'New York', '2th avenue'),
+       ('Jordan', 'Mike', '987-654-3210', 01212, 'Washington', 'South Park 3'),
+       ('Durak', 'Stephen', '555-666-7777', 55654, 'Miami', 'Sea Drive 112');
 
 
 --================== Varify =====================
@@ -31,17 +31,17 @@ jingjingyang@jingjings-MacBook-Pro ~ % sqlite3 test.db
 SQLite version 3.39.5 2022-10-14 20:58:05
 Enter ".help" for usage hints.
 
-sqlite> INSERT INTO person(id, last, first, phone, zip, city, address)
-   ...> VALUES (1, 'Doe', 'John', 123455, 11122, 'New York', '2th avenue'),
-   ...>        (2, 'Jordan', 'Mike', 221122, 01212, 'Washington', 'South Park 3'),
-   ...>        (3, 'Durak', 'Stephen', 550011, 55654, 'Miami', 'Sea Drive 112');
-   
+sqlite> INSERT INTO person(last, first, phone, zip, city, address)
+   ...> VALUES ('Doe', 'John', '123-456-7890', 11122, 'New York', '2th avenue'),
+   ...>        ('Jordan', 'Mike', '987-654-3210', 01212, 'Washington', 'South Park 3'),
+   ...>        ('Durak', 'Stephen', '555-666-7777', 55654, 'Miami', 'Sea Drive 112');
+
 sqlite> SELECT * FROM person;
-id  last    first    phone   zip    city        address      
---  ------  -------  ------  -----  ----------  -------------
-1   Doe     John     123455  11122  New York    2th avenue   
-2   Jordan  Mike     221122  1212   Washington  South Park 3 
-3   Durak   Stephen  550011  55654  Miami       Sea Drive 112
+id  last    first    phone         zip    city        address      
+--  ------  -------  ------------  -----  ----------  -------------
+1   Doe     John     123-456-7890  11122  New York    2th avenue   
+2   Jordan  Mike     987-654-3210  1212   Washington  South Park 3 
+3   Durak   Stephen  555-666-7777  55654  Miami       Sea Drive 112
 */
 
 -- End of file
