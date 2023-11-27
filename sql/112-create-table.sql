8d34dea5c561eeb8f71f57ac92c3a3ebd5c2a466
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 00:17:10 +0200

    sql/112: modify homework

--- sql/112-create-table.sql
+++ sql/112-create-table.sql
@@ -1,29 +1,26 @@
 -- Author: Jingjing Yang <jingjing.yang@tuni.fi>
 -- Date: 2023-11-15
 -- File: 112-create-table.sql
-
 --================== Task ======================
 /*
-    11.2 Create table 2
+11.2 Create table 2
 
-        Create table for football teams (do not insert any data).
-        Coach first name, coach last name, team name and comment.
+Create table for football teams (do not insert any data).
+Coach first name, coach last name, team name and comment.
 
-            table: football
+table: football
 
-            id first last team comment
-            -- ----- ---- ---- -------
-*/
-
-
-CREATE TABLE football (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    first VARCHAR(255),
-    last VARCHAR(255),
-    team VARCHAR(255),
-    comment VARCHAR(255)
-);
-
+id first last team comment
+-- ----- ---- ---- -------
+ */
+CREATE TABLE
+    football (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        first VARCHAR(255),
+        last VARCHAR(255),
+        team VARCHAR(255),
+        comment VARCHAR(255)
+    );
 
 --================== Varify =====================
 /*
@@ -33,13 +30,13 @@ SQLite version 3.39.5 2022-10-14 20:58:05
 Enter ".help" for usage hints.
 
 sqlite> CREATE TABLE football (
-   ...>     id SERIAL PRIMARY KEY,
-   ...>     first VARCHAR(255),
-   ...>     last VARCHAR(255),
-   ...>     team VARCHAR(255),
-   ...>     comment VARCHAR(255)
-   ...> );
-   
+...>     id SERIAL PRIMARY KEY,
+...>     first VARCHAR(255),
+...>     last VARCHAR(255),
+...>     team VARCHAR(255),
+...>     comment VARCHAR(255)
+...> );
+
 sqlite> PRAGMA table_info(football);
 cid  name     type          notnull  dflt_value  pk
 ---  -------  ------------  -------  ----------  --
@@ -52,13 +49,13 @@ cid  name     type          notnull  dflt_value  pk
 sqlite> DROP TABLE football;
 
 sqlite> CREATE TABLE football (
-   ...>     id INTEGER PRIMARY KEY AUTOINCREMENT,
-   ...>     first VARCHAR(255),
-   ...>     last VARCHAR(255),
-   ...>     team VARCHAR(255),
-   ...>     comment VARCHAR(255)
-   ...> );
-   
+...>     id INTEGER PRIMARY KEY AUTOINCREMENT,
+...>     first VARCHAR(255),
+...>     last VARCHAR(255),
+...>     team VARCHAR(255),
+...>     comment VARCHAR(255)
+...> );
+
 sqlite> PRAGMA table_info(football);
 cid  name     type          notnull  dflt_value  pk
 ---  -------  ------------  -------  ----------  --
@@ -67,6 +64,5 @@ cid  name     type          notnull  dflt_value  pk
 2    last     VARCHAR(255)  0        NULL        0 
 3    team     VARCHAR(255)  0        NULL        0 
 4    comment  VARCHAR(255)  0        NULL        0 
-*/
-
+ */
 -- End of file
