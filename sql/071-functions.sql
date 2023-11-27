437ee562d6a337d9ebd20aa6427d67ee87713ca3
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 00:29:36 +0200

    sql/071: modify homework

--- sql/071-functions.sql
+++ sql/071-functions.sql
@@ -1,29 +1,31 @@
 -- Author: Jingjing Yang <jingjing.yang@tuni.fi>
 -- Date: 2023-11-15
 -- File: 071-functions.sql
-
 --================== Task ======================
 /*
-    7.1 Functions 1
+7.1 Functions 1
 
-        Write a query to display all results for all employees in
-        lowercase. Order the reults by name (alphabetically) and job
-        (alphabetically).
+Write a query to display all results for all employees in
+lowercase. Order the reults by name (alphabetically) and job
+(alphabetically).
 
-            Name       Job          Salary
-            ---------- --------- ---------
-            ...
-            blake      manager        2850
-            ...
-            king       president      5000
-            ...
-*/
-
-
-SELECT LOWER(ename) AS "Name", LOWER(job) AS "Job", sal AS "Salary"
-FROM emp
-ORDER BY "Name", "Job";
-
+Name       Job          Salary
+---------- --------- ---------
+...
+blake      manager        2850
+...
+king       president      5000
+...
+ */
+SELECT
+    LOWER(ename) AS "Name",
+    LOWER(job) AS "Job",
+    sal AS "Salary"
+FROM
+    emp
+ORDER BY
+    "Name",
+    "Job";
 
 --================== Varify =====================
 /*
@@ -34,8 +36,8 @@ SQLite version 3.39.5 2022-10-14 20:58:05
 Enter ".help" for usage hints.
 
 sqlite> SELECT LOWER(ename) AS "Name", LOWER(job) AS "Job", sal AS "Salary"
-   ...> FROM emp
-   ...> ORDER BY "Name", "Job";
+...> FROM emp
+...> ORDER BY "Name", "Job";
 Name    Job        Salary
 ------  ---------  ------
 adams   clerk      1100  
@@ -52,6 +54,5 @@ scott   analyst    3000
 smith   clerk      800   
 turner  salesman   1500  
 ward    salesman   1250  
-*/
-
+ */
 -- End of file
