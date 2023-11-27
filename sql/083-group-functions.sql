24b4058c3e73d84478e775d9853f5e7591a2ef4b
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 01:15:05 +0200

    sql/083: modify homework

--- sql/083-group-functions.sql
+++ sql/083-group-functions.sql
@@ -9,12 +9,15 @@ Display the highest and lowest salaries for employees
 including departments 10 and 30. Label the columns "high" and "low".
  */
 SELECT
+   deptno,
    MAX(sal) AS "high",
    MIN(sal) AS "low"
 FROM
    emp
 WHERE
-   deptno IN (10, 30);
+   deptno IN (10, 30)
+GROUP BY
+   deptno;
 
 --================== Varify =====================
 /*
@@ -44,5 +47,37 @@ sqlite> SELECT MAX(sal) AS "high", MIN(sal) AS "low"
 high  low
 ----  ---
 5000  950
+
+sqlite> SELECT sal
+...> FROM emp
+...> WHERE deptno=10
+...> ORDER BY sal;
+sal 
+----
+1300
+2450
+5000
+
+sqlite> SELECT sal
+...> FROM emp
+...> WHERE deptno=30
+...> ORDER BY sal;
+sal 
+----
+950 
+1250
+1250
+1500
+1600
+2850
+
+sqlite> SELECT deptno, MAX(sal) AS "high", MIN(sal) AS "low"
+...> FROM emp
+...> WHERE deptno IN (10, 30);
+...> GROUP BY deptno;
+deptno  high  low 
+------  ----  ----
+10      5000  1300
+30      2850  950 
  */
 -- End of file
