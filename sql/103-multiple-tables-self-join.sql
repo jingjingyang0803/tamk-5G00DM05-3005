4baa5597bc5f74efe5010b81320dc063db97d6a0
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 00:16:21 +0200

    sql/103: modify homework

--- sql/103-multiple-tables-self-join.sql
+++ sql/103-multiple-tables-self-join.sql
@@ -1,22 +1,22 @@
 -- Author: Jingjing Yang <jingjing.yang@tuni.fi>
 -- Date: 2023-11-15
 -- File: 103-multiple-tables-self-join.sql
-
 --================== Task ======================
 /*
-    10.3 Multiple tables and self join 3
+10.3 Multiple tables and self join 3
 
-        Display all employee names who earn more than their managers.
-        Ignore commission. Order the results alphabetically.
-*/
-
-
-SELECT e.ename
-FROM emp e
-JOIN emp m ON e.mgr = m.empno
-WHERE e.sal > m.sal
-ORDER BY e.ename;
-
+Display all employee names who earn more than their managers.
+Ignore commission. Order the results alphabetically.
+ */
+SELECT
+   e.ename
+FROM
+   emp e
+   JOIN emp m ON e.mgr = m.empno
+WHERE
+   e.sal > m.sal
+ORDER BY
+   e.ename;
 
 --================== Varify =====================
 /*
@@ -44,19 +44,19 @@ empno  ename   job        mgr   hiredate    sal   comm  deptno
 7934   MILLER  CLERK      7782  1982-01-23  1300  NULL  10  
 
 sqlite> SELECT e.ename
-   ...> FROM emp e
-   ...> JOIN emp m ON e.mgr = m.empno
-   ...> WHERE e.sal > m.sal
-   ...> ORDER BY e.ename;
+...> FROM emp e
+...> JOIN emp m ON e.mgr = m.empno
+...> WHERE e.sal > m.sal
+...> ORDER BY e.ename;
 ename
 -----
 FORD 
 SCOTT
 
 sqlite> SELECT e.ename AS "Employee", e.sal AS "Employee Salary", m.ename AS "Manager", m.sal AS "Manager Salary"
-   ...> FROM emp e
-   ...> JOIN emp m ON e.mgr = m.empno
-   ...> ORDER BY e.sal;
+...> FROM emp e
+...> JOIN emp m ON e.mgr = m.empno
+...> ORDER BY e.sal;
 Employee  Employee Salary  Manager  Manager Salary
 --------  ---------------  -------  --------------
 SMITH     800              FORD     3000          
@@ -72,6 +72,5 @@ BLAKE     2850             KING     5000
 JONES     2975             KING     5000          
 SCOTT     3000             JONES    2975          
 FORD      3000             JONES    2975 
-*/
-
+ */
 -- End of file
