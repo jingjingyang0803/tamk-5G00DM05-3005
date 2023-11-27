12df84164bed39a322790b40f7f8ddf53236aa29
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 00:12:36 +0200

    sql/082: modify homework

--- sql/082-group-functions.sql
+++ sql/082-group-functions.sql
@@ -1,20 +1,19 @@
 -- Author: Jingjing Yang <jingjing.yang@tuni.fi>
 -- Date: 2023-11-15
 -- File: 082-group-functions.sql
-
 --================== Task ======================
 /*
-    8.2 Group functions 2
+8.2 Group functions 2
 
-        How many employees work in department 30? Label the column
-        "count of dept 30",
-*/
-
-
-SELECT COUNT(DISTINCT empno) AS "count of dept 30"
-FROM emp
-WHERE deptno = 30;
-
+How many employees work in department 30? Label the column
+"count of dept 30",
+ */
+SELECT
+   COUNT(DISTINCT empno) AS "count of dept 30"
+FROM
+   emp
+WHERE
+   deptno = 30;
 
 --================== Varify =====================
 /*
@@ -22,9 +21,9 @@ jingjingyang@jingjings-MacBook-Pro ~ % sqlite3 test.db
 -- Loading resources from /Users/jingjingyang/.sqliterc
 SQLite version 3.39.5 2022-10-14 20:58:05
 Enter ".help" for usage hints. 
-              
+
 sqlite> SELECT * FROM emp 
-   ...> WHERE deptno = 30;
+...> WHERE deptno = 30;
 empno  ename   job       mgr   hiredate    sal   comm  deptno
 -----  ------  --------  ----  ----------  ----  ----  ------
 7499   ALLEN   SALESMAN  7698  1981-02-20  1600  300   30    
@@ -35,11 +34,10 @@ empno  ename   job       mgr   hiredate    sal   comm  deptno
 7900   JAMES   CLERK     7698  1981-12-03  950   NULL  30   
 
 sqlite> SELECT COUNT(DISTINCT empno) AS "count of dept 30"
-   ...> FROM emp
-   ...> WHERE deptno = 30;
+...> FROM emp
+...> WHERE deptno = 30;
 count of dept 30
 ----------------
 6 
-*/
-
+ */
 -- End of file
