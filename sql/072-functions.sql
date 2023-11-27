824a16a1af20832cb1668b68cea17207722eaca4
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 00:07:38 +0200

    sql/072: modify homework

--- sql/072-functions.sql
+++ sql/072-functions.sql
@@ -1,37 +1,38 @@
 -- Author: Jingjing Yang <jingjing.yang@tuni.fi>
 -- Date: 2023-11-15
 -- File: 072-functions.sql
-
 --================== Task ======================
 /*
-    7.2 Functions 2
+7.2 Functions 2
 
-        Write query to display following report for all employees.
-        Pay attention to the _order_ of the displayed rows.
+Write query to display following report for all employees.
+Pay attention to the _order_ of the displayed rows.
 
-            Name        Characters
-            ----------  ----------
-            ford        4
-            king        4
-            ward        4
-            adams       5
-            allen       5
-            blake       5
-            clark       5
-            james       5
-            jones       5
-            scott       5
-            smith       5
-            martin      6
-            miller      6
-            turner      6
-*/
-
-
-SELECT LOWER(ename) AS Name, LENGTH(ename) AS Characters
-FROM emp
-ORDER BY Characters, Name;
-
+Name        Characters
+----------  ----------
+ford        4
+king        4
+ward        4
+adams       5
+allen       5
+blake       5
+clark       5
+james       5
+jones       5
+scott       5
+smith       5
+martin      6
+miller      6
+turner      6
+ */
+SELECT
+    LOWER(ename) AS "Name",
+    LENGTH (ename) AS "Characters"
+FROM
+    emp
+ORDER BY
+    "Characters",
+    "Name";
 
 --================== Varify =====================
 /*
@@ -40,9 +41,9 @@ jingjingyang@jingjings-MacBook-Pro ~ % sqlite3 test.db
 SQLite version 3.39.5 2022-10-14 20:58:05
 Enter ".help" for usage hints.
 
-sqlite> SELECT LOWER(ename) AS Name, LENGTH(ename) AS Characters
-   ...> FROM emp
-   ...> ORDER BY Characters, Name;
+sqlite> SELECT LOWER(ename) AS "Name", LENGTH(ename) AS "Characters"
+...> FROM emp
+...> ORDER BY "Characters", "Name";
 Name    Characters
 ------  ----------
 ford    4         
@@ -59,6 +60,5 @@ smith   5
 martin  6         
 miller  6         
 turner  6 
-*/
-
+ */
 -- End of file
