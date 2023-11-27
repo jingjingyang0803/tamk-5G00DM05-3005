c626e81cdac1635b924c717a5044c9f499649505
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 00:27:21 +0200

    sql/042: modify homework

--- sql/042-like.sql
+++ sql/042-like.sql
@@ -1,49 +1,52 @@
 -- Author: Jingjing Yang <jingjing.yang@tuni.fi>
 -- Date: 2023-11-14
 -- File: 042-like.sql
-
 --================== Task ======================
 /*
-    4.2 Like 2
+4.2 Like 2
 
-        Display name and department of every employees who have a
-        *vowel* (aeiouy or in uppercase) at the second character and
-        whose work description includes sales or analysis. Sort the
-        results by the department number (lowest first) and person
-        name (alphabetically). Display:
+Display name and department of every employees who have a
+ *vowel* (aeiouy or in uppercase) at the second character and
+whose work description includes sales or analysis. Sort the
+results by the department number (lowest first) and person
+name (alphabetically). Display:
 
-            Name       Department
-            ---------- ----------
-            KING               10
-            MILLER             10
-            FORD               20
-            JONES              20
-            JAMES              30
-            ...
-             |
-             Look at character 2, which must be a vowel
-*/
-
-
-SELECT ename AS Name, deptno AS Department
-FROM emp
-WHERE ename REGEXP '^.[aeiouyAEIOUY]' AND (job LIKE '%sales%' OR job LIKE '%analysis%')
-ORDER BY deptno ASC, ename ASC;
+Name       Department
+---------- ----------
+KING               10
+MILLER             10
+FORD               20
+JONES              20
+JAMES              30
+...
+|
+Look at character 2, which must be a vowel
+ */
+SELECT
+   ename AS Name,
+   deptno AS Department
+FROM
+   emp
+WHERE
+   ename REGEXP '^.[aeiouyAEIOUY]'
+   AND (
+      job LIKE '%sales%'
+      OR job LIKE '%analysis%'
+   )
+ORDER BY
+   deptno ASC,
+   ename ASC;
 
 -- In SQLite, the LIKE operator is case-insensitive by default. 
 -- This means it does not differentiate between upper and lower case.
 -- so why we need to use: LOWER(job) LIKE '%sales%'?
-
-
 --================== Alternative Solution =====================
 /*
 SELECT ename AS Name, deptno AS Department
 FROM emp
 WHERE (ename LIKE '_a%' OR ename LIKE '_e%' OR ename LIKE '_i%' OR ename LIKE '_o%' OR ename LIKE '_u%' OR ename LIKE '_y%') AND (job LIKE '%sales%' OR job LIKE '%analysis%')
 ORDER BY deptno ASC, ename ASC;
-*/
-
-
+ */
 --================== Varify =====================
 /*
 jingjingyang@jingjings-MacBook-Pro ~ % sqlite3 test.db
@@ -64,7 +67,7 @@ cid  name      type          notnull  dflt_value  pk
 7    deptno    INTEGER       1        NULL        0 
 
 sqlite> SELECT DISTINCT ename
-   ...> FROM emp;
+...> FROM emp;
 ename 
 ------
 SMITH 
@@ -83,7 +86,7 @@ FORD
 MILLER
 
 sqlite> SELECT DISTINCT job
-   ...> FROM emp;
+...> FROM emp;
 job      
 ---------
 CLERK    
@@ -93,9 +96,9 @@ ANALYST
 PRESIDENT
 
 sqlite> SELECT ename AS Name, deptno AS Department
-   ...> FROM emp
-   ...> WHERE (ename LIKE '_a%' OR ename LIKE '_e%' OR ename LIKE '_i%' OR ename LIKE '_o%' OR ename LIKE '_u%' OR ename LIKE '_y%')
-   ...> ORDER BY deptno ASC, ename ASC;
+...> FROM emp
+...> WHERE (ename LIKE '_a%' OR ename LIKE '_e%' OR ename LIKE '_i%' OR ename LIKE '_o%' OR ename LIKE '_u%' OR ename LIKE '_y%')
+...> ORDER BY deptno ASC, ename ASC;
 Name    Department
 ------  ----------
 KING    10        
@@ -108,9 +111,9 @@ TURNER  30
 WARD    30
 
 sqlite> SELECT ename AS Name, deptno AS Department
-   ...> FROM emp
-   ...> WHERE (ename LIKE '_a%' OR ename LIKE '_e%' OR ename LIKE '_i%' OR ename LIKE '_o%' OR ename LIKE '_u%' OR ename LIKE '_y%') AND (job LIKE '%sales%' OR job LIKE '%analysis%')
-   ...> ORDER BY deptno ASC, ename ASC;
+...> FROM emp
+...> WHERE (ename LIKE '_a%' OR ename LIKE '_e%' OR ename LIKE '_i%' OR ename LIKE '_o%' OR ename LIKE '_u%' OR ename LIKE '_y%') AND (job LIKE '%sales%' OR job LIKE '%analysis%')
+...> ORDER BY deptno ASC, ename ASC;
 Name    Department
 ------  ----------
 MARTIN  30        
@@ -118,14 +121,13 @@ TURNER  30
 WARD    30 
 
 sqlite> SELECT ename AS Name, deptno AS Department
-   ...> FROM emp
-   ...> WHERE ename REGEXP '^.[aeiouyAEIOUY]' AND (job LIKE '%sales%' OR job LIKE '%analysis%')
-   ...> ORDER BY deptno ASC, ename ASC;
+...> FROM emp
+...> WHERE ename REGEXP '^.[aeiouyAEIOUY]' AND (job LIKE '%sales%' OR job LIKE '%analysis%')
+...> ORDER BY deptno ASC, ename ASC;
 Name    Department
 ------  ----------
 MARTIN  30        
 TURNER  30        
 WARD    30 
-*/
-
+ */
 -- End of file
