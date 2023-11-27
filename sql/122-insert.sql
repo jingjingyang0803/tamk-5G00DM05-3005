92f64b37beeaabc16058373344c82246eec5c02b
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 00:18:28 +0200

    sql/122: modify homework

--- sql/122-insert.sql
+++ sql/122-insert.sql
@@ -1,28 +1,36 @@
 -- Author: Jingjing Yang <jingjing.yang@tuni.fi>
 -- Date: 2023-11-15
 -- File: 122-insert.sql
-
 --================== Task ======================
 /*
-    12.2 Insert 2
+12.2 Insert 2
 
-        Insert data to football teams.
+Insert data to football teams.
 
-            table: football
+table: football
 
-            id first  last     team        comment
-            -- -----  -------  ----------- ----------------------
-             1 Jim    Anders   Manchester  UPS 10
-             2 Mark   Knofles  Liverpool   Rockwell international
-             3 Jenny  Richards Highland    Scottish national
-*/
-
-
-INSERT INTO football(first, last, team, comment)
-VALUES ('Jim', 'Anders', 'Manchester', 'UPS 10'),
-       ('Mark', 'Knofles', 'Liverpool', 'Rockwell international'),
-       ('Jenny', 'Richards', 'Highland', 'Scottish national');
-
+id first  last     team        comment
+-- -----  -------  ----------- ----------------------
+1 Jim    Anders   Manchester  UPS 10
+2 Mark   Knofles  Liverpool   Rockwell international
+3 Jenny  Richards Highland    Scottish national
+ */
+INSERT INTO
+    football (first, last, team, comment)
+VALUES
+    ('Jim', 'Anders', 'Manchester', 'UPS 10'),
+    (
+        'Mark',
+        'Knofles',
+        'Liverpool',
+        'Rockwell international'
+    ),
+    (
+        'Jenny',
+        'Richards',
+        'Highland',
+        'Scottish national'
+    );
 
 --================== Varify =====================
 /*
@@ -32,16 +40,15 @@ SQLite version 3.39.5 2022-10-14 20:58:05
 Enter ".help" for usage hints.
 
 sqlite> INSERT INTO football(first, last, team, comment)
-   ...> VALUES ('Jim', 'Anders', 'Manchester', 'UPS 10'),
-   ...>        ('Mark', 'Knofles', 'Liverpool', 'Rockwell international'),
-   ...>        ('Jenny', 'Richards', 'Highland', 'Scottish national');
-   
+...> VALUES ('Jim', 'Anders', 'Manchester', 'UPS 10'),
+...>        ('Mark', 'Knofles', 'Liverpool', 'Rockwell international'),
+...>        ('Jenny', 'Richards', 'Highland', 'Scottish national');
+
 sqlite> SELECT * FROM football;
 id  first  last      team        comment               
 --  -----  --------  ----------  ----------------------
 1   Jim    Anders    Manchester  UPS 10                
 2   Mark   Knofles   Liverpool   Rockwell international
 3   Jenny  Richards  Highland    Scottish national   
-*/
-
+ */
 -- End of file
