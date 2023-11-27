6bece1a5f61c1a60d3385cbf6b884eefdcb7bea8
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 00:21:16 +0200

    sql/014: modify homework

--- sql/014-query.sql
+++ sql/014-query.sql
@@ -1,24 +1,23 @@
 -- Author: Jingjing Yang <jingjing.yang@tuni.fi>
 -- Date: 2023-11-14
 -- File: 014-query.sql
-
 --================== Task ======================
 /*
 
-    1.4 Query 2
+1.4 Query 2
 
-        Display name and calculated salaries for all
-        employees for the first quarter of the year. Ignore
-        commissions. Display:
+Display name and calculated salaries for all
+employees for the first quarter of the year. Ignore
+commissions. Display:
 
-                ename  first quarter
-                -----  -------------
-*/
-
-
-SELECT ename, (sal * 3) AS "first quarter"
-FROM emp;
-
+ename  first quarter
+-----  -------------
+ */
+SELECT
+    ename,
+    (sal * 3) AS "first quarter"
+FROM
+    emp;
 
 --================== Varify =====================
 /*
@@ -38,7 +37,7 @@ cid  name      type          notnull  dflt_value  pk
 6    comm      NUMERIC(7,2)  0        NULL        0 
 7    deptno    INTEGER       1        NULL        0 
 sqlite> SELECT ename, (sal * 3) AS "first quarter"
-   ...> FROM emp;
+...> FROM emp;
 ename   first quarter
 ------  -------------
 SMITH   2400         
@@ -55,6 +54,5 @@ ADAMS   3300
 JAMES   2850         
 FORD    9000         
 MILLER  3900 
-*/
-
+ */
 -- End of file
