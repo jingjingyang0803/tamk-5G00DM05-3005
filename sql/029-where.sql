e80bfffdd16bc8015f794da5cb9fc0db5a797c5a
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 14:38:35 +0200

    sql/029: modify homework

--- sql/029-where.sql
+++ sql/029-where.sql
@@ -3,12 +3,12 @@
 -- File: 029-where.sql
 
 --================== Task ======================
-/*
-2.9 Where 9
+-- /*
+-- 2.9 Where 9
 
-[NULL value handling] Display the employee name and job for
-all employees who do not have a manager.
-*/
+-- [NULL value handling] Display the employee name and job for
+-- all employees who do not have a manager.
+-- */
 
 
 SELECT
@@ -21,42 +21,42 @@ WHERE
 
 
 --================== Varify =====================
-/*
-jingjingyang@jingjings-MacBook-Pro ~ % sqlite3 test.db
--- Loading resources from /Users/jingjingyang/.sqliterc
-SQLite version 3.39.5 2022-10-14 20:58:05
-Enter ".help" for usage hints.
+-- /*
+-- jingjingyang@jingjings-MacBook-Pro ~ % sqlite3 test.db
+-- -- Loading resources from /Users/jingjingyang/.sqliterc
+-- SQLite version 3.39.5 2022-10-14 20:58:05
+-- Enter ".help" for usage hints.
 
-sqlite> PRAGMA table_info(emp);
-cid  name      type          notnull  dflt_value  pk
----  --------  ------------  -------  ----------  --
-0    empno     INTEGER       1        NULL        1 
-1    ename     VARCHAR(10)   0        NULL        0 
-2    job       VARCHAR(9)    0        NULL        0 
-3    mgr       INTEGER       0        NULL        0 
-4    hiredate  DATE          0        NULL        0 
-5    sal       NUMERIC(7,2)  0        NULL        0 
-6    comm      NUMERIC(7,2)  0        NULL        0 
-7    deptno    INTEGER       1        NULL        0 
+-- sqlite> PRAGMA table_info(emp);
+-- cid  name      type          notnull  dflt_value  pk
+-- ---  --------  ------------  -------  ----------  --
+-- 0    empno     INTEGER       1        NULL        1 
+-- 1    ename     VARCHAR(10)   0        NULL        0 
+-- 2    job       VARCHAR(9)    0        NULL        0 
+-- 3    mgr       INTEGER       0        NULL        0 
+-- 4    hiredate  DATE          0        NULL        0 
+-- 5    sal       NUMERIC(7,2)  0        NULL        0 
+-- 6    comm      NUMERIC(7,2)  0        NULL        0 
+-- 7    deptno    INTEGER       1        NULL        0 
 
-sqlite> SELECT DISTINCT mgr
-...> FROM emp;
-mgr 
-----
-7902
-7698
-7839
-7566
-NULL
-7788
-7782
+-- sqlite> SELECT DISTINCT mgr
+-- ...> FROM emp;
+-- mgr 
+-- ----
+-- 7902
+-- 7698
+-- 7839
+-- 7566
+-- NULL
+-- 7788
+-- 7782
 
-sqlite> SELECT ename, job
-...> FROM emp
-...> WHERE mgr IS NULL;
-ename  job      
------  ---------
-KING   PRESIDENT
-*/
+-- sqlite> SELECT ename, job
+-- ...> FROM emp
+-- ...> WHERE mgr IS NULL;
+-- ename  job      
+-- -----  ---------
+-- KING   PRESIDENT
+-- */
 
 -- End of file
