ff17ab0ba5209a2c56d483214f6090451dc03b65
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 16:23:23 +0200

    sql/123: modify homework

--- sql/123-insert.sql
+++ sql/123-insert.sql
@@ -20,24 +20,13 @@
 
 
 INSERT INTO
-    weather (time_of_reading, high, low, sig, comment)
+    weather (id, time_of_reading, high, low, sig, comment)
 VALUES
-    (
-        '2000-12-11 06:00',
-        5.5,
-        1.5,
-        'MK',
-        'Snow-rain for a while'
-    ),
-    ('2000-12-12 06:00', 4.1, 0.3, 'ES', 'Strong wind'),
-    (
-        '2000-12-14 06:00',
-        2.0,
-        -5.8,
-        'RD',
-        'Night was cold, foggy weather'
-    ),
-    ('2000-12-15 06:00', 12.8, 7.5, 'MK', 'Rainy day');
+    (1, '2000-12-11 06:00', 5.5, 1.5, 'MK', 'Snow-rain for a while'),
+    (2, '2000-12-12 06:00', 4.1, 0.3, 'ES', 'Strong wind'),
+    (3, '2000-12-14 06:00', 2.0, -5.8, 'RD', 'Night was cold, foggy weather'),
+    (4, '2000-12-15 06:00', 12.8, 7.5, 'MK', 'Rainy day');
+
 
 --================== Varify =====================
 -- /*
