fc0347bb73fe446f3faae9c65a07a9d713757803
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 14:52:04 +0200

    sql/124: modify homework

--- sql/124-insert.sql
+++ sql/124-insert.sql
@@ -3,19 +3,19 @@
 -- File: 124-insert.sql
 
 --================== Task ======================
-/*
-12.4 Insert 4
+-- /*
+-- 12.4 Insert 4
 
-Insert data to inventory:
+-- Insert data to inventory:
 
-table: inventory
+-- table: inventory
 
-id item      worth comment
--- --------  ----- -------------------
-1 baseball  150   My first baseball
-2 bike      2000  For mountain riding
-3 playcards 10    Funny pictures
-*/
+-- id item      worth comment
+-- -- --------  ----- -------------------
+-- 1 baseball  150   My first baseball
+-- 2 bike      2000  For mountain riding
+-- 3 playcards 10    Funny pictures
+-- */
 
 
 INSERT INTO
@@ -27,23 +27,23 @@ VALUES
 
 
 --================== Varify =====================
-/*
-jingjingyang@jingjings-MacBook-Pro ~ % sqlite3 test.db
--- Loading resources from /Users/jingjingyang/.sqliterc
-SQLite version 3.39.5 2022-10-14 20:58:05
-Enter ".help" for usage hints.
+-- /*
+-- jingjingyang@jingjings-MacBook-Pro ~ % sqlite3 test.db
+-- -- Loading resources from /Users/jingjingyang/.sqliterc
+-- SQLite version 3.39.5 2022-10-14 20:58:05
+-- Enter ".help" for usage hints.
 
-sqlite> INSERT INTO inventory(item, worth, comment)
-...> VALUES ('baseball', 150, 'My first baseball'),
-...>        ('bike', 2000, 'For mountain riding'),
-...>        ('playcards', 10, 'Funny pictures');
+-- sqlite> INSERT INTO inventory(item, worth, comment)
+-- ...> VALUES ('baseball', 150, 'My first baseball'),
+-- ...>        ('bike', 2000, 'For mountain riding'),
+-- ...>        ('playcards', 10, 'Funny pictures');
 
-sqlite> SELECT * FROM inventory;
-id  item       worth  comment            
---  ---------  -----  -------------------
-1   baseball   150    My first baseball  
-2   bike       2000   For mountain riding
-3   playcards  10     Funny pictures     
-*/
+-- sqlite> SELECT * FROM inventory;
+-- id  item       worth  comment            
+-- --  ---------  -----  -------------------
+-- 1   baseball   150    My first baseball  
+-- 2   bike       2000   For mountain riding
+-- 3   playcards  10     Funny pictures     
+-- */
 
 -- End of file
