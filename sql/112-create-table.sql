6b7e22dbd6d21bb41e6f43292e0d9b1695bc9b05
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 14:50:11 +0200

    sql/112: modify homework

--- sql/112-create-table.sql
+++ sql/112-create-table.sql
@@ -3,17 +3,17 @@
 -- File: 112-create-table.sql
 
 --================== Task ======================
-/*
-11.2 Create table 2
+-- /*
+-- 11.2 Create table 2
 
-Create table for football teams (do not insert any data).
-Coach first name, coach last name, team name and comment.
+-- Create table for football teams (do not insert any data).
+-- Coach first name, coach last name, team name and comment.
 
-table: football
+-- table: football
 
-id first last team comment
--- ----- ---- ---- -------
-*/
+-- id first last team comment
+-- -- ----- ---- ---- -------
+-- */
 
 
 CREATE TABLE
@@ -27,47 +27,47 @@ CREATE TABLE
 
 
 --================== Varify =====================
-/*
-jingjingyang@jingjings-MacBook-Pro ~ % sqlite3 test.db
--- Loading resources from /Users/jingjingyang/.sqliterc
-SQLite version 3.39.5 2022-10-14 20:58:05
-Enter ".help" for usage hints.
+-- /*
+-- jingjingyang@jingjings-MacBook-Pro ~ % sqlite3 test.db
+-- -- Loading resources from /Users/jingjingyang/.sqliterc
+-- SQLite version 3.39.5 2022-10-14 20:58:05
+-- Enter ".help" for usage hints.
 
-sqlite> CREATE TABLE football (
-...>     id SERIAL PRIMARY KEY,
-...>     first VARCHAR(255),
-...>     last VARCHAR(255),
-...>     team VARCHAR(255),
-...>     comment VARCHAR(255)
-...> );
+-- sqlite> CREATE TABLE football (
+-- ...>     id SERIAL PRIMARY KEY,
+-- ...>     first VARCHAR(255),
+-- ...>     last VARCHAR(255),
+-- ...>     team VARCHAR(255),
+-- ...>     comment VARCHAR(255)
+-- ...> );
 
-sqlite> PRAGMA table_info(football);
-cid  name     type          notnull  dflt_value  pk
----  -------  ------------  -------  ----------  --
-0    id       SERIAL        0        NULL        1 
-1    first    VARCHAR(255)  0        NULL        0 
-2    last     VARCHAR(255)  0        NULL        0 
-3    team     VARCHAR(255)  0        NULL        0 
-4    comment  VARCHAR(255)  0        NULL        0 
+-- sqlite> PRAGMA table_info(football);
+-- cid  name     type          notnull  dflt_value  pk
+-- ---  -------  ------------  -------  ----------  --
+-- 0    id       SERIAL        0        NULL        1 
+-- 1    first    VARCHAR(255)  0        NULL        0 
+-- 2    last     VARCHAR(255)  0        NULL        0 
+-- 3    team     VARCHAR(255)  0        NULL        0 
+-- 4    comment  VARCHAR(255)  0        NULL        0 
 
-sqlite> DROP TABLE football;
+-- sqlite> DROP TABLE football;
 
-sqlite> CREATE TABLE football (
-...>     id INTEGER PRIMARY KEY AUTOINCREMENT,
-...>     first VARCHAR(255),
-...>     last VARCHAR(255),
-...>     team VARCHAR(255),
-...>     comment VARCHAR(255)
-...> );
+-- sqlite> CREATE TABLE football (
+-- ...>     id INTEGER PRIMARY KEY AUTOINCREMENT,
+-- ...>     first VARCHAR(255),
+-- ...>     last VARCHAR(255),
+-- ...>     team VARCHAR(255),
+-- ...>     comment VARCHAR(255)
+-- ...> );
 
-sqlite> PRAGMA table_info(football);
-cid  name     type          notnull  dflt_value  pk
----  -------  ------------  -------  ----------  --
-0    id       INTEGER       0        NULL        1 
-1    first    VARCHAR(255)  0        NULL        0 
-2    last     VARCHAR(255)  0        NULL        0 
-3    team     VARCHAR(255)  0        NULL        0 
-4    comment  VARCHAR(255)  0        NULL        0 
-*/
+-- sqlite> PRAGMA table_info(football);
+-- cid  name     type          notnull  dflt_value  pk
+-- ---  -------  ------------  -------  ----------  --
+-- 0    id       INTEGER       0        NULL        1 
+-- 1    first    VARCHAR(255)  0        NULL        0 
+-- 2    last     VARCHAR(255)  0        NULL        0 
+-- 3    team     VARCHAR(255)  0        NULL        0 
+-- 4    comment  VARCHAR(255)  0        NULL        0 
+-- */
 
 -- End of file
