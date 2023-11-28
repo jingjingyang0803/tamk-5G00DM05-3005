6c966beae77625f87a19d97cdb9e1cd3a1be8691
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 14:54:09 +0200

    sql/083: modify homework

--- sql/083-group-functions.sql
+++ sql/083-group-functions.sql
@@ -3,12 +3,12 @@
 -- File: 083-group-functions.sql
 
 --================== Task ======================
-/*
-8.3 Group functions 3
+-- /*
+-- 8.3 Group functions 3
 
-Display the highest and lowest salaries for employees
-including departments 10 and 30. Label the columns "high" and "low".
-*/
+-- Display the highest and lowest salaries for employees
+-- including departments 10 and 30. Label the columns "high" and "low".
+-- */
 
 
 SELECT
@@ -24,65 +24,65 @@ GROUP BY
 
 
 --================== Varify =====================
-/*
-jingjingyang@jingjings-MacBook-Pro ~ % sqlite3 test.db
--- Loading resources from /Users/jingjingyang/.sqliterc
-SQLite version 3.39.5 2022-10-14 20:58:05
-Enter ".help" for usage hints.
+-- /*
+-- jingjingyang@jingjings-MacBook-Pro ~ % sqlite3 test.db
+-- -- Loading resources from /Users/jingjingyang/.sqliterc
+-- SQLite version 3.39.5 2022-10-14 20:58:05
+-- Enter ".help" for usage hints.
 
-sqlite> SELECT DISTINCT sal
-...> FROM emp
-...> WHERE deptno IN (10, 30)
-...> ORDER BY sal;
-sal 
-----
-950 
-1250
-1300
-1500
-1600
-2450
-2850
-5000
+-- sqlite> SELECT DISTINCT sal
+-- ...> FROM emp
+-- ...> WHERE deptno IN (10, 30)
+-- ...> ORDER BY sal;
+-- sal 
+-- ----
+-- 950 
+-- 1250
+-- 1300
+-- 1500
+-- 1600
+-- 2450
+-- 2850
+-- 5000
 
-sqlite> SELECT MAX(sal) AS "high", MIN(sal) AS "low"
-...> FROM emp
-...> WHERE deptno IN (10, 30);
-high  low
-----  ---
-5000  950
+-- sqlite> SELECT MAX(sal) AS "high", MIN(sal) AS "low"
+-- ...> FROM emp
+-- ...> WHERE deptno IN (10, 30);
+-- high  low
+-- ----  ---
+-- 5000  950
 
-sqlite> SELECT sal
-...> FROM emp
-...> WHERE deptno=10
-...> ORDER BY sal;
-sal 
-----
-1300
-2450
-5000
+-- sqlite> SELECT sal
+-- ...> FROM emp
+-- ...> WHERE deptno=10
+-- ...> ORDER BY sal;
+-- sal 
+-- ----
+-- 1300
+-- 2450
+-- 5000
 
-sqlite> SELECT sal
-...> FROM emp
-...> WHERE deptno=30
-...> ORDER BY sal;
-sal 
-----
-950 
-1250
-1250
-1500
-1600
-2850
+-- sqlite> SELECT sal
+-- ...> FROM emp
+-- ...> WHERE deptno=30
+-- ...> ORDER BY sal;
+-- sal 
+-- ----
+-- 950 
+-- 1250
+-- 1250
+-- 1500
+-- 1600
+-- 2850
 
-sqlite> SELECT deptno, MAX(sal) AS "high", MIN(sal) AS "low"
-...> FROM emp
-...> WHERE deptno IN (10, 30);
-...> GROUP BY deptno;
-deptno  high  low 
-------  ----  ----
-10      5000  1300
-30      2850  950 
-*/
+-- sqlite> SELECT deptno, MAX(sal) AS "high", MIN(sal) AS "low"
+-- ...> FROM emp
+-- ...> WHERE deptno IN (10, 30);
+-- ...> GROUP BY deptno;
+-- deptno  high  low 
+-- ------  ----  ----
+-- 10      5000  1300
+-- 30      2850  950 
+-- */
 
 -- End of file
