89ba45f2b400794dbc4153f859b61bafb0760577
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 14:51:25 +0200

    sql/122: modify homework

--- sql/122-insert.sql
+++ sql/122-insert.sql
@@ -3,19 +3,19 @@
 -- File: 122-insert.sql
 
 --================== Task ======================
-/*
-12.2 Insert 2
+-- /*
+-- 12.2 Insert 2
 
-Insert data to football teams.
+-- Insert data to football teams.
 
-table: football
+-- table: football
 
-id first  last     team        comment
--- -----  -------  ----------- ----------------------
-1 Jim    Anders   Manchester  UPS 10
-2 Mark   Knofles  Liverpool   Rockwell international
-3 Jenny  Richards Highland    Scottish national
-*/
+-- id first  last     team        comment
+-- -- -----  -------  ----------- ----------------------
+-- 1 Jim    Anders   Manchester  UPS 10
+-- 2 Mark   Knofles  Liverpool   Rockwell international
+-- 3 Jenny  Richards Highland    Scottish national
+-- */
 
 
 INSERT INTO
@@ -37,23 +37,23 @@ VALUES
 
 
 --================== Varify =====================
-/*
-jingjingyang@jingjings-MacBook-Pro ~ % sqlite3 test.db
--- Loading resources from /Users/jingjingyang/.sqliterc
-SQLite version 3.39.5 2022-10-14 20:58:05
-Enter ".help" for usage hints.
+-- /*
+-- jingjingyang@jingjings-MacBook-Pro ~ % sqlite3 test.db
+-- -- Loading resources from /Users/jingjingyang/.sqliterc
+-- SQLite version 3.39.5 2022-10-14 20:58:05
+-- Enter ".help" for usage hints.
 
-sqlite> INSERT INTO football(first, last, team, comment)
-...> VALUES ('Jim', 'Anders', 'Manchester', 'UPS 10'),
-...>        ('Mark', 'Knofles', 'Liverpool', 'Rockwell international'),
-...>        ('Jenny', 'Richards', 'Highland', 'Scottish national');
+-- sqlite> INSERT INTO football(first, last, team, comment)
+-- ...> VALUES ('Jim', 'Anders', 'Manchester', 'UPS 10'),
+-- ...>        ('Mark', 'Knofles', 'Liverpool', 'Rockwell international'),
+-- ...>        ('Jenny', 'Richards', 'Highland', 'Scottish national');
 
-sqlite> SELECT * FROM football;
-id  first  last      team        comment               
---  -----  --------  ----------  ----------------------
-1   Jim    Anders    Manchester  UPS 10                
-2   Mark   Knofles   Liverpool   Rockwell international
-3   Jenny  Richards  Highland    Scottish national   
-*/
+-- sqlite> SELECT * FROM football;
+-- id  first  last      team        comment               
+-- --  -----  --------  ----------  ----------------------
+-- 1   Jim    Anders    Manchester  UPS 10                
+-- 2   Mark   Knofles   Liverpool   Rockwell international
+-- 3   Jenny  Richards  Highland    Scottish national   
+-- */
 
 -- End of file
