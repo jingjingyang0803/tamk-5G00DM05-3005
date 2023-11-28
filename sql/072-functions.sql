8abe33b2617b07677f12a71afa98f5004910fed8
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 14:44:29 +0200

    sql/072: modify homework

--- sql/072-functions.sql
+++ sql/072-functions.sql
@@ -3,29 +3,29 @@
 -- File: 072-functions.sql
 
 --================== Task ======================
-/*
-7.2 Functions 2
+-- /*
+-- 7.2 Functions 2
 
-Write query to display following report for all employees.
-Pay attention to the _order_ of the displayed rows.
+-- Write query to display following report for all employees.
+-- Pay attention to the _order_ of the displayed rows.
 
-Name        Characters
-----------  ----------
-ford        4
-king        4
-ward        4
-adams       5
-allen       5
-blake       5
-clark       5
-james       5
-jones       5
-scott       5
-smith       5
-martin      6
-miller      6
-turner      6
-*/
+-- Name        Characters
+-- ----------  ----------
+-- ford        4
+-- king        4
+-- ward        4
+-- adams       5
+-- allen       5
+-- blake       5
+-- clark       5
+-- james       5
+-- jones       5
+-- scott       5
+-- smith       5
+-- martin      6
+-- miller      6
+-- turner      6
+-- */
 
 
 SELECT
@@ -39,31 +39,31 @@ ORDER BY
 
 
 --================== Varify =====================
-/*
-jingjingyang@jingjings-MacBook-Pro ~ % sqlite3 test.db
--- Loading resources from /Users/jingjingyang/.sqliterc
-SQLite version 3.39.5 2022-10-14 20:58:05
-Enter ".help" for usage hints.
+-- /*
+-- jingjingyang@jingjings-MacBook-Pro ~ % sqlite3 test.db
+-- -- Loading resources from /Users/jingjingyang/.sqliterc
+-- SQLite version 3.39.5 2022-10-14 20:58:05
+-- Enter ".help" for usage hints.
 
-sqlite> SELECT LOWER(ename) AS "Name", LENGTH(ename) AS "Characters"
-...> FROM emp
-...> ORDER BY "Characters", "Name";
-Name    Characters
-------  ----------
-ford    4         
-king    4         
-ward    4         
-adams   5         
-allen   5         
-blake   5         
-clark   5         
-james   5         
-jones   5         
-scott   5         
-smith   5         
-martin  6         
-miller  6         
-turner  6 
-*/
+-- sqlite> SELECT LOWER(ename) AS "Name", LENGTH(ename) AS "Characters"
+-- ...> FROM emp
+-- ...> ORDER BY "Characters", "Name";
+-- Name    Characters
+-- ------  ----------
+-- ford    4         
+-- king    4         
+-- ward    4         
+-- adams   5         
+-- allen   5         
+-- blake   5         
+-- clark   5         
+-- james   5         
+-- jones   5         
+-- scott   5         
+-- smith   5         
+-- martin  6         
+-- miller  6         
+-- turner  6 
+-- */
 
 -- End of file
