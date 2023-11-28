97e5c7697b4eb1d5c703f7b57033bcdd75d34e82
Jingjing Yang <jingjing.yang@tuni.fi>
2023-11-28 15:55:49 +0200

    sql/104: modify homework

--- sql/104-multiple-tables-self-join.sql
+++ sql/104-multiple-tables-self-join.sql
@@ -22,9 +22,9 @@ SELECT
    e.ename AS "Employee",
    m.ename AS "Manager"
 FROM
-   emp e
-   JOIN emp m ON e.mgr = m.empno
-   JOIN dept d ON e.deptno = d.deptno
+   emp AS e
+   JOIN emp AS m ON e.mgr = m.empno
+   JOIN dept AS d ON e.deptno = d.deptno
 WHERE
    UPPER(m.ename) IN ('BLAKE', 'FORD', 'JONES')
    AND m.sal > (
@@ -36,9 +36,9 @@ WHERE
          grade = 3
    )
 ORDER BY
-   "Location",
-   "Manager",
-   "Employee";
+   "Location" ASC,
+   "Manager" ASC,
+   "Employee" ASC;
 
 
 --================== Varify =====================
