f8192b1471219fd7b647335769dd6e596d5074c9
Jingjing Yang <jingjing.yang@tuni.fi>
2023-12-19 19:31:29 +0200

    add another endpoint for searching

--- project/README.md
+++ project/README.md
@@ -8,8 +8,6 @@ Jingjing Yang
 
 Date of creation: 16th November 2023
 
-Date of last update:
-
 ## Setup and Run
 
 1. Install Node.js and npm.
@@ -21,8 +19,7 @@ Date of last update:
 4. Run `npm install` to install all dependencies.
 5. Setup SQLite database by running SQL scripts.
    - Run `sqlite3 database.db < sql/create.sql` to create the Clothes table.
-   - Run `sqlite3 database.db < sql/delete.sql` to delete all data from the Clothes table.
-   - Run `sqlite3 database.db < sql/insert.sql` to insert data into the Clothes table.
+   - Run `sqlite3 database.db < sql/insert.sql` to insert some sample data into the Clothes table.
 6. Run `node server.js` to start the server.
 7. The server runs on `http://localhost:8080`.
 
@@ -36,43 +33,71 @@ Server is running on http://localhost:8080 ...
 ## Endpoints
 
 - GET /clothes: Retrieves all clothing items.
-- GET /clothes/:id: Retrieves a specific clothing item by ID.
+- GET /clothes/:id: Retrieves a specific clothing item by ID.-
+- GET /search?name={name}: Searches for clothing items by name.
+- GET /search?size={size}&color={color}: Searches for clothing items by size and color.
 - POST /clothes: Adds a new clothing item.
 - PUT /clothes/:id: Updates an existing clothing item with the given ID.
 - DELETE /clothes/:id: Deletes the clothing item with the given ID.
 
 ## Example Usage
 
-Using curl to interact with the API:
+Using curl to interact with the API, you can add option -i (or --include) to include the HTTP response headers in the output, useful for debugging.
+Alternatively, add -s (or --silent) to silence curl's progress output, making it more succinct.
 
 - GET all clothing items: `curl http://localhost:8080/clothes`
 - GET a specific item: `curl http://localhost:8080/clothes/1`
+- Search by name: `curl -i http://localhost:8080/search?name={name}`
+- Search by size and color: `curl http://localhost:8080/search?size={size}&color={color}`
 - POST a new item: `curl -X POST -H "Content-Type: application/json" -d '{"name":"Wool Sweater","type":"Sweater","size":"L","color":"Red","price":60.99}' http://localhost:8080/clothes`
 - PUT to update an item: `curl -X PUT -H "Content-Type: application/json" -d '{"name":"Denim Jacket","type":"Jacket","size":"XL","color":"Brown","price":50.99}' http://localhost:8080/clothes/1`
 - DELETE an item: `curl -X DELETE http://localhost:8080/clothes/1`
 
 ```bash
+# root
 jingjingyang@jingjings-MacBook-Pro ~ % curl http://localhost:8080
 Found. Redirecting to /clothes%
+
+# get all
 jingjingyang@jingjings-MacBook-Pro ~ % curl http://localhost:8080/clothes
 [{"id":1,"name":"Denim Jacket","type":"Jacket","size":"M","color":"Blue","price":59.99},{"id":2,"name":"Leather Boots","type":"Shoes","size":"8","color":"Black","price":89.99},{"id":3,"name":"Floral Dress","type":"Dress","size":"S","color":"Multicolor","price":45.99},{"id":4,"name":"Cotton T-Shirt","type":"Shirt","size":"L","color":"White","price":15.99}]%
 
+# get by id
 jingjingyang@jingjings-MacBook-Pro ~ % curl http://localhost:8080/clothes/1
 {"id":1,"name":"Denim Jacket","type":"Jacket","size":"M","color":"Blue","price":59.99}%
 
+# search by name
+(base) jingjingyang@jingjings-MacBook-Pro ~ % curl 'http://localhost:8080/search?name=boot'
+[{"id":2,"name":"Leather Boots","type":"Shoes","size":"8","color":"Black","price":89.99}]%
+
+# search by name
+(base) jingjingyang@jingjings-MacBook-Pro ~ % curl 'http://localhost:8080/search?name=dress'
+[{"id":3,"name":"Floral Dress","type":"Dress","size":"S","color":"Multicolor","price":45.99}]%
+
+# search by size and color
+(base) jingjingyang@jingjings-MacBook-Pro ~ % curl 'http://localhost:8080/search?size=8&color=black'
+[{"id":2,"name":"Leather Boots","type":"Shoes","size":"8","color":"Black","price":89.99}]%
+
+# add a new item
 jingjingyang@jingjings-MacBook-Pro ~ % curl -X POST -H "Content-Type: application/json" -d '{"name":"Wool Sweater","type":"Sweater","size":"L","color":"Red","price":60.99}' http://localhost:8080/clothes
 {"id":5}%
+
 jingjingyang@jingjings-MacBook-Pro ~ % curl http://localhost:8080/clothes
 [{"id":1,"name":"Denim Jacket","type":"Jacket","size":"M","color":"Blue","price":59.99},{"id":2,"name":"Leather Boots","type":"Shoes","size":"8","color":"Black","price":89.99},{"id":3,"name":"Floral Dress","type":"Dress","size":"S","color":"Multicolor","price":45.99},{"id":4,"name":"Cotton T-Shirt","type":"Shirt","size":"L","color":"White","price":15.99},{"id":5,"name":"Wool Sweater","type":"Sweater","size":"L","color":"Red","price":60.99}]%
 
+# update a item
 jingjingyang@jingjings-MacBook-Pro ~ % curl -X PUT -H "Content-Type: application/json" -d '{"name":"Denim Jacket","type":"Jacket","size":"XL","color":"Brown","price":50.99}' http://localhost:8080/clothes/1
 {"message":"Row updated","changes":1}%
+
 jingjingyang@jingjings-MacBook-Pro ~ % curl http://localhost:8080/clothes/1
 {"id":1,"name":"Denim Jacket","type":"Jacket","size":"XL","color":"Brown","price":50.99}%
 
+# delete a item
 jingjingyang@jingjings-MacBook-Pro ~ % curl -X DELETE http://localhost:8080/clothes/1
 {"message":"Row deleted","changes":1}%
+
 jingjingyang@jingjings-MacBook-Pro ~ % curl http://localhost:8080/clothes/1
+
 jingjingyang@jingjings-MacBook-Pro ~ % curl http://localhost:8080/clothes
 [{"id":2,"name":"Leather Boots","type":"Shoes","size":"8","color":"Black","price":89.99},{"id":3,"name":"Floral Dress","type":"Dress","size":"S","color":"Multicolor","price":45.99},{"id":4,"name":"Cotton T-Shirt","type":"Shirt","size":"L","color":"White","price":15.99},{"id":5,"name":"Wool Sweater","type":"Sweater","size":"L","color":"Red","price":60.99}]%
 ```
--- project/server.js
+++ project/server.js
@@ -7,17 +7,19 @@ let db = new sqlite3.Database("./database.db"); // Connect to SQLite database
 const table = "Clothes"; // Define table name
 
 // Define HTTP status codes
-const HTTP_STATUS_OK = 200;
-const HTTP_STATUS_CREATED = 201;
-const HTTP_STATUS_NO_CONTENT = 204;
-const HTTP_STATUS_INTERNAL_SERVER_ERROR = 500;
+const HTTP_STATUS_OK = 200; // OK status, request succeeded
+const HTTP_STATUS_CREATED = 201; // Request succeeded and a new resource was created as a result
+const HTTP_STATUS_NO_CONTENT = 204; // Request succeeded, but there's no representation to return (i.e. the response is empty)
+const HTTP_STATUS_INTERNAL_SERVER_ERROR = 500; // Server encountered an unexpected condition which prevented it from fulfilling the request
 
 // Define SQL queries
-const SELECT_ALL = `SELECT * FROM ${table}`;
-const SELECT_ID = `SELECT * FROM ${table} WHERE id = ?`;
-const INSERT_INTO = `INSERT INTO ${table} (name, type, size, color, price) VALUES (?, ?, ?, ?, ?)`;
-const UPDATE = `UPDATE ${table} SET name = ?, type = ?, size = ?, color = ?, price = ? WHERE id = ?`;
-const DELETE = `DELETE FROM ${table} WHERE id = ?`;
+const SELECT_ALL = `SELECT * FROM ${table}`; // SQL query to select all records from table
+const SELECT_ID = `SELECT * FROM ${table} WHERE id = ?`; // SQL query to select record from table by id
+const SEARCH_BY_NAME = `SELECT * FROM ${table} WHERE name LIKE ?`; // SQL query to find records by name
+const SEARCH_BY_SIZE_AND_COLOR = `SELECT * FROM ${table} WHERE LOWER(size) = LOWER(?) AND LOWER(color) = LOWER(?)`; // SQL query to find records by size and color
+const INSERT_INTO = `INSERT INTO ${table} (name, type, size, color, price) VALUES (?, ?, ?, ?, ?)`; // SQL query to insert new record into table
+const UPDATE = `UPDATE ${table} SET name = ?, type = ?, size = ?, color = ?, price = ? WHERE id = ?`; // SQL query to update a record in the table
+const DELETE = `DELETE FROM ${table} WHERE id = ?`; // SQL query to delete a record from the table by id
 
 // Function to create error response
 function createErrorResponse(err) {
@@ -25,7 +27,10 @@ function createErrorResponse(err) {
     error: err.message, // Error message
     code: err.code, // Error code
     details: {
-      /* any relevant details */
+      method: req.method, // HTTP method of the request
+      url: req.url, // Requested URL
+      headers: req.headers, // Headers sent with the request
+      body: req.body, // Body of the request
     },
     stack: process.env.NODE_ENV === "development" ? err.stack : "hidden", // Stack trace (only in development)
   };
@@ -68,6 +73,51 @@ app.get(`/clothes/:id`, (req, res) => {
   });
 });
 
+// Endpoint to handle GET requests to '/clothes'
+app.get(`/search`, (req, res) => {
+  // Check if name query parameter is given
+  if (req.query.name) {
+    // Store query parameter in an array
+    const values = [`%${req.query.name}%`];
+
+    // Run SQL query to search by name
+    db.all(SEARCH_BY_NAME, values, (err, rows) => {
+      // If there is an error, send error response
+      if (err) {
+        res
+          .status(HTTP_STATUS_INTERNAL_SERVER_ERROR)
+          .json(createErrorResponse(err));
+        return;
+      }
+      // If no error, send rows returned by the query as response
+      res.json(rows);
+    });
+  }
+  // Check if both size and color query parameters are given
+  else if (req.query.size && req.query.color) {
+    // Store query parameters in an array
+    const values = [req.query.size, req.query.color];
+
+    // Run SQL query to search by size and color
+    db.all(SEARCH_BY_SIZE_AND_COLOR, values, (err, rows) => {
+      // If there is an error, send error response
+      if (err) {
+        res
+          .status(HTTP_STATUS_INTERNAL_SERVER_ERROR)
+          .json(createErrorResponse(err));
+        return;
+      }
+      // If no error, send rows returned by the query as response
+      res.json(rows);
+    });
+  }
+
+  // If neither size and color nor name query parameters are given, send error message
+  else {
+    res.status(400).json({ message: "Invalid query parameters" });
+  }
+});
+
 app.post(`/clothes`, (req, res) => {
   // POST endpoint
   const values = [
@@ -81,58 +131,61 @@ app.post(`/clothes`, (req, res) => {
   db.run(INSERT_INTO, values, function (err) {
     // Execute SQL query
     if (err) {
-      // If error occurs
+      // If error occurs, return 500 status (Internal Server Error) and error details
       res
         .status(HTTP_STATUS_INTERNAL_SERVER_ERROR)
-        .json(createErrorResponse(err)); // Send error response
+        .json(createErrorResponse(err));
       return;
     }
-    res.status(HTTP_STATUS_CREATED).json({ id: this.lastID }); // Send response with created ID
+    // If no error, return 201 status (Created) and the ID of the newly created record
+    res.status(HTTP_STATUS_CREATED).json({ id: this.lastID });
   });
 });
 
 app.put(`/clothes/:id`, (req, res) => {
-  // PUT endpoint
+  // PUT endpoint for updating a record
   const values = [
-    req.body.name, // Values from request body and ID from URL
+    req.body.name, // Values from request body
     req.body.type,
     req.body.size,
     req.body.color,
     req.body.price,
-    req.params.id,
+    req.params.id, // ID from URL
   ];
 
   db.run(UPDATE, values, function (err) {
     // Execute SQL query
     if (err) {
-      // If error occurs
+      // If error occurs, return 500 status (Internal Server Error) and error details
       res
         .status(HTTP_STATUS_INTERNAL_SERVER_ERROR)
-        .json(createErrorResponse(err)); // Send error response
+        .json(createErrorResponse(err));
       return;
     }
+    // If no error, return 200 status (OK) and a message indicating the update was successful
     res
       .status(HTTP_STATUS_OK)
-      .json({ message: "Row updated", changes: this.changes }); // Send response with update message
+      .json({ message: "Row updated", changes: this.changes });
   });
 });
 
 app.delete(`/clothes/:id`, (req, res) => {
-  // DELETE endpoint
-  const values = [req.params.id]; // ID parameter from URL
+  // DELETE endpoint for deleting a record
+  const values = [req.params.id]; // ID from URL
 
   db.run(DELETE, values, function (err) {
     // Execute SQL query
     if (err) {
-      // If error occurs
+      // If error occurs, return 500 status (Internal Server Error) and error details
       res
         .status(HTTP_STATUS_INTERNAL_SERVER_ERROR)
-        .json(createErrorResponse(err)); // Send error response
+        .json(createErrorResponse(err));
       return;
     }
+    // If no error, return 204 status (No Content) and a message indicating the deletion was successful
     res
       .status(HTTP_STATUS_NO_CONTENT)
-      .json({ message: "Row deleted", changes: this.changes }); // Send response with delete message
+      .json({ message: "Row deleted", changes: this.changes });
   });
 });
 
